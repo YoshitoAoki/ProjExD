9f0f01bbad90d46169773bf6defc7e1dc63ac9f1
Aoki <c0a2100387@edu.teu.ac.jp>
2022-11-29 14:43:33 +0900

    練習問題６

--- ex02/practice.py
+++ ex02/practice.py
@@ -4,7 +4,8 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     num = btn["text"]
-    tkm.showinfo("",f"{num}ボタンがクリックされました")
+    # tkm.showinfo("",f"{num}ボタンがクリックされました")
+    entry.insert(tk.END,num)
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -16,7 +17,7 @@ w,h = 1,0
 for b in range(9,-1,-1):
     button = tk.Button(root,text=b,font=("",30),width=4,height=2)
     button.grid(row=w,column=h )
-    # button.bind("<1>",button_click)
+    button.bind("<1>",button_click)
     h+=1
     if h%3==0:
         w+=1
