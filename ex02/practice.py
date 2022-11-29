64ca41ac1f1ccb3c1f5d980f3fda6172221014c8
Aoki <c0a2100387@edu.teu.ac.jp>
2022-11-29 14:38:08 +0900

    練習問題４

--- ex02/practice.py
+++ ex02/practice.py
@@ -8,11 +8,15 @@ def button_click(event):
 
 root = tk.Tk()
 root.geometry("300x500")
-w,h = 0,0
+
+entry = tk.Entry(root,justify="right",width=10,font=("",40))
+entry.grid(row=0,column=0,columnspan=3)
+
+w,h = 1,0
 for b in range(9,-1,-1):
     button = tk.Button(root,text=b,font=("",30),width=4,height=2)
     button.grid(row=w,column=h )
-    button.bind("<1>",button_click)
+    # button.bind("<1>",button_click)
     h+=1
     if h%3==0:
         w+=1
