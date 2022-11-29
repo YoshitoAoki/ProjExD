b61b4bf9fb5cb5a2b324657b868e53fc825f3b0c
Aoki <c0a2100387@edu.teu.ac.jp>
2022-11-29 14:26:38 +0900

    練習問題２

--- ex02/practice.py
+++ ex02/practice.py
@@ -2,3 +2,13 @@ import tkinter as tk
 
 root = tk.Tk()
 root.geometry("300x500")
+w,h = 0,0
+for b in range(9,-1,-1):
+    button = tk.Button(root,text=b,font=("",30),width=4,height=2)
+    button.grid(row=w,column=h )
+    h+=1
+    if h%3==0:
+        w+=1
+        h=0
+
+root.mainloop()
