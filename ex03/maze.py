397ae5ef3c847519afeadaa812d48db768957dec
Aoki <c0a2100387@edu.teu.ac.jp>
2022-12-06 14:45:53 +0900

    練習問題６，７

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,22 +4,32 @@ def key_down(event):
     global key
     key = event.keysym
 
+
 def key_up(event):
     global key
     key = ""
 
+
+def main_proc():
+    global cx, cy
+    if key == "Up": cy -= 20
+    if key == "Down": cy += 20
+    if key == "Left": cx -= 20
+    if key == "Right": cx += 20
+    canvas.coords("kokaton", cx, cy)
+    root.after(100, main_proc)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
-    canvas = tk.Canvas(root,width=1500,height=900,bg="black")
-
-    koukaton = tk.PhotoImage(file="fig/8.png")
-    cx,cy = 300,400
-    canvas.create_image(cx,cy,image=koukaton,tag="こうかとん")
-
-    key = ""
-    root.bind("<KeyPress>",key_down)
-    root.bind("<KeyRelease>",key_up)
-
+    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
+    cx, cy = 300, 400
+    tori = tk.PhotoImage(file="fig/8.png")
+    canvas.create_image(cx, cy, image=tori, tag="kokaton")
+    key = ""
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+    main_proc()
     root.mainloop()
