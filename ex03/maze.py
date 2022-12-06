05ee8ffe35b90f200d51fcc358cd5d44614babc7
Aoki <c0a2100387@edu.teu.ac.jp>
2022-12-06 16:11:57 +0900

    time実装

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,8 @@ import maze_maker as mm
 def key_down(event):
     global key
     key = event.keysym
+    if key == "s" and flag == 0:
+        start()
 
 
 def key_up(event):
@@ -26,12 +28,37 @@ def main_proc():
     canvas.coords("kokaton", cx, cy)
     root.after(200, main_proc)
 
+def timer():
+    global tmr
+    label["text"] = f"Time:{tmr}秒"
+    tmr += 1
+    jid = root.after(1000,timer)
+
+def start():
+    global flag
+    flag = 1
+    if key == "s":
+        timer()
+        main_proc()
+
+
+
+
+# def goal():
+#     pass
+        
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
+
+    flag = 0
+    tmr = 0
+    label = tk.Label(root,font=("",45),fg="black",bg="white")
+    label.place(x=1150,y=20)
 
     maze_lst = mm.make_maze(15,9)
     mm.show_maze(canvas,maze_lst)
@@ -44,8 +71,6 @@ if __name__ == "__main__":
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
-    main_proc()
     
     
-
     root.mainloop()
