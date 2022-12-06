ad1653d7d4524c9b748ca8ccf95df996835bea56
Aoki <c0a2100387@edu.teu.ac.jp>
2022-12-06 14:28:38 +0900

    練習問題３

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,5 +4,10 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root,width=1500,height=900,bg="black")
+
+    koukaton = tk.PhotoImage(file="fig/8.png")
+    cx,cy = 300,400
+    canvas.create_image(cx,cy,image=koukaton,tag="こうかとん")
+
     canvas.pack()
     root.mainloop()
