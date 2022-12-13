4fe4f2106abe42a334e004757e55d8fd0518ac45
Aoki <c0a2100387@edu.teu.ac.jp>
2022-12-13 14:16:51 +0900

    練習問題２

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,9 +2,19 @@ import pygame as pg
 import sys
 
 def main():
-    scrn_sfc = pg.display.set_mode((1600,900))
-    bgpic = pg.image.load("ex04/pg_bg.jpg")
+    clock = pg.time.Clock()
+    scrn_sfc = pg.display.set_mode((1400,700))
+    pgbg_sfc = pg.image.load("ex04/pg_bg.jpg")
+    pgbg_rct = pgbg_sfc.get_rect()
     
+    while True:
+        scrn_sfc.blit(pgbg_sfc,pgbg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        
+        pg.display.update()
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
