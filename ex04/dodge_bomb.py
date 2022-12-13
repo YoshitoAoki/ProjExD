47b9d5b9a99d1c7b41b38f0af2121e0c8ba61706
Aoki <c0a2100387@edu.teu.ac.jp>
2022-12-13 14:04:49 +0900

    練習問題１

---
+++ ex04/dodge_bomb.py
@@ -0,0 +1,14 @@
+import pygame as pg
+import sys
+
+def main():
+    scrn_sfc = pg.display.set_mode((1600,900))
+    bgpic = pg.image.load("ex04/pg_bg.jpg")
+    
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
